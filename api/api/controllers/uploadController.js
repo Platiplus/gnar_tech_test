41e8c407674d87eecdc38e6c6df2ab8892425a71
Platiplus <quazarweb@gmail.com>
2020-10-17 13:36:26 -0300

    Get all uploads route implemented and error handling

--- api/api/controllers/uploadController.js
+++ api/api/controllers/uploadController.js
@@ -7,20 +7,34 @@ const uploadFile = async (request, response) => {
 
 // GET INFO ABOUT ALL THE UPLOADS
 const getAllUploadedInfo = async (request, response) => {
+  try {
+    const uploads = await Upload.findAll()
 
+    return response.status(200).json({ error: false, data: uploads })
+  } catch (error) {
+    return response.status(500).json({ error: true, data: error.message })
+  }
 }
 
 // GET THE CONTENTS OF ONE FILE THAT HAS BEEN UPLOADED
 const getOneUploadedFile = async (request, response) => {
-  const uploadId = request.params.uploadId
+  try {
+    const uploadId = request.params.uploadId
 
-  const upload = await Upload.findByPk(uploadId, {
-    include: {
-      association: 'upload_info'
+    const upload = await Upload.findByPk(uploadId, {
+      include: {
+        association: 'upload_info'
+      }
+    })
+
+    if (upload == null) {
+      return response.status(404).json({ error: true, data: `There is no file uploaded with id ${uploadId}` })
     }
-  })
 
-  return response.status(200).json({ error: false, data: upload })
+    return response.status(200).json({ error: false, data: upload })
+  } catch (error) {
+    return response.status(500).json({ error: true, data: error.message })
+  }
 }
 
 module.exports = {
