5e1fcf587455fea9bfa4b60d2f0da96b78597840
Platiplus <quazarweb@gmail.com>
2020-10-17 13:19:11 -0300

    Implementing method to get uploaded_info from id_upload

--- api/api/controllers/uploadController.js
+++ api/api/controllers/uploadController.js
@@ -1,14 +1,26 @@
+const Upload = require('../models/Upload')
+
 // UPLOAD A NEW FILE TO THE SERVER
 const uploadFile = async (request, response) => {
 
 }
+
 // GET INFO ABOUT ALL THE UPLOADS
 const getAllUploadedInfo = async (request, response) => {
 
 }
+
 // GET THE CONTENTS OF ONE FILE THAT HAS BEEN UPLOADED
 const getOneUploadedFile = async (request, response) => {
+  const uploadId = request.params.uploadId
 
+  const upload = await Upload.findByPk(uploadId, {
+    include: {
+      association: 'upload_info'
+    }
+  })
+
+  return response.status(200).json({ error: false, data: upload })
 }
 
 module.exports = {
